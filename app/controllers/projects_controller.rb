906de77da83db0cb26873561e78ee616599c277d
Scott Mascio <scottmascio@gmail.com>
2013-12-11 16:26:25 -0600

    added validations

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,7 @@ class ProjectsController < ApplicationController
 
   def new
     @project = Project.new
+    @project.save
   end
 
   def create
@@ -12,7 +13,8 @@ class ProjectsController < ApplicationController
       flash[:notice] = "Project has been created."
       redirect_to @project
     else
-      # nothing, yet
+      flash[:alert] = "Project has not been created."
+      render "new"
     end
   end
 
